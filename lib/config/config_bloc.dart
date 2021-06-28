bb7539246a31a7ac62a7adb469ad24225581e629
romajc <joeyroman84@gmail.com>
2021-06-28 14:33:33 -0400

    problems with bloc

---
+++ lib/config/config_bloc.dart
@@ -0,0 +1,11 @@
+// import 'package:bloc/bloc.dart';
+
+// class ConfigBloc extends Bloc<ConfigEvent, ConfigState> {
+//   @override
+//   ConfigState get initialState =>
+
+//   @override
+//   Stream<ConfigState> mapEventToState(ConfigEvent event,
+//   ) async* {
+//   }
+// }
